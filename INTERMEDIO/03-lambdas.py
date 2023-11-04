cf4120e1e4c0174940e08aebe20aac00b642f0ae
Andres Quintero <quinte05@gmail.com>
2023-11-04 14:34:30 -0500

    clase 3 - lambdas

---
+++ INTERMEDIO/03-lambdas.py
@@ -0,0 +1,25 @@
+### Lambdas ###
+
+def sum_two_values(
+    first_value, second_value): return first_value + second_value
+
+
+print(sum_two_values(2, 4))
+
+
+def multiply_values(
+    first_value, second_value): return first_value * second_value - 3
+
+
+print(multiply_values(2, 4))
+
+
+def sum_three_values(value):
+    return lambda first_value, second_value: first_value + second_value + value
+
+
+print(sum_three_values(5)(2, 4))
+
+sumar = lambda n1, n2: n1 + n2
+
+print(sumar(5,5))
