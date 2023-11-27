6ebc9a7d836a8f5cf266e7a61d6dcbceb5c8ba44
Andres Quintero <quinte05@gmail.com>
2023-11-27 09:58:51 -0500

    se agrega conexion a bd remota

--- BACKEND/FastAPI/db/client.py
+++ BACKEND/FastAPI/db/client.py
@@ -1,3 +1,7 @@
 from pymongo import MongoClient
 
-db_client = MongoClient().local
+#Base de Datos Local
+#db_client = MongoClient().local
+
+#Base de Datos Remota
+db_client = MongoClient("mongodb+srv://quinte05:ThmKNr5NBDMCOv3Z@cluster0.ov0rqh2.mongodb.net/?retryWrites=true&w=majority").test
